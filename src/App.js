ca10fc9bff7c602e000111a6c6dca15019a1ca46
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-08-09 16:42:35 +0530

    Display new tab on clicking on Image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 
 import './App.css';
 import React from 'react';
-import logo from 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/ReactJs/helloworld/src/assests/Neeraj-Chopra.jpg';
+import image from 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/ReactJs/helloworld/src/assests/Neeraj-Chopra.jpg';
 
 class App extends React.Component{
+url = "https://en.wikipedia.org/wiki/Neeraj_Chopra"
+
  constructor(){
    super()
    this.state = {
      title: <div>
             <h2>"Hello I am Practising React Component"</h2>
-            "Hello Harshit"
+            "Hello Neeraj Chopra"
      </div>
-   }
+    }
  }
- render(){
-  return(
-    <div>
-       <h1>{this.state.title}</h1>
-       <img src={logo} alt='neerajchopra'/>
-    </div>
-   
-  );
-}
+
+ shoWebsite =() =>{
+   window.open(this.url,"_blank");
+ }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.title}</h1>
+        <img src={image} alt='neerajchopra' onClick={this.shoWebsite}/>
+      </div>
+    );
+  }
 }
 
 export default App;
